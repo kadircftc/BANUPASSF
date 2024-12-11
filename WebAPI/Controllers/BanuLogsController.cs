9a8b6deb162f36bc7b03b33aaf91d33688c6239e
Muhammed Ali Öztürk <Muhammed_Ali95@outlook.com.tr>
2024-12-12 01:46:50 +0300

    added pdfConverterService and banuLogsPdfQuery

--- WebAPI/Controllers/BanuLogsController.cs
+++ WebAPI/Controllers/BanuLogsController.cs
@@ -1,6 +1,7 @@
 ﻿
 using Business.Handlers.BanuLogs.Commands;
 using Business.Handlers.BanuLogs.Queries;
+using Business.Services.ConvertPdfService.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Entities.Concrete;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace WebAPI.Controllers
 {
@@ -51,6 +53,63 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> GetById(Guid id)
         {
             var result = await Mediator.Send(new GetBanuLogQuery { Id = id });
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [Produces("application/pdf", "text/plain")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BanuLog))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        [HttpGet("getbypdf")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetByDate(String date)
+        {
+            var result = await Mediator.Send(new GetBanuLogsByPdfQuery { QueryDate = date });
+            if (result.Success)
+            {   
+                ConvertPdfService convertPdfService = new ConvertPdfService();
+                if (result.Data.Count() > 35)
+                {
+                    var logGroups = convertPdfService.SplitLogs(result.Data, 35);
+
+                    var zipBytes = convertPdfService.GenerateZipWithPdfs(logGroups, date);
+
+                    return File(zipBytes, "application/zip", $"BanuLogs_{date}.zip");
+                }
+                else
+                {
+                    var pdfBytes = convertPdfService.GeneratePdf(result.Data.ToList(), 1, date);
+
+                    return File(pdfBytes, "application/pdf", $"BanuLogs_{date}.pdf");
+                }
+            }
+
+            return BadRequest(result.Message);
+        } 
+        
+        
+        ///<summary>
+        ///It brings the details according to its full name.
+        ///</summary>
+        ///<remarks>BanuLogs</remarks>
+        ///<return>BanuLogs List By Fullname</return>
+        ///<response code="200"></response>  
+        [Produces("application/json", "text/plain")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<BanuLog>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+        [HttpGet("getbyfullname")]
+        
+        public async Task<IActionResult> GetByPersonelFullName(string personelFullName, string? queryStartDate=null, string? queryEndDate = null)
+        {
+            var result = await Mediator.Send(new GetBanuLogsByFullNameQuery
+            {
+                FullName = personelFullName,
+                QueryStartDate = queryStartDate,
+                QueryEndDate = queryEndDate
+            });
             if (result.Success)
             {
                 return Ok(result.Data);
